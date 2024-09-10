69e46efdc9e1ade0101f3d51e5476c6c2a3c75e3
priyanjan <priyanjanjb@gmail.com>
2024-09-10 09:49:46 +0530

    delete results page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'homePage.dart';
+import 'package:tmtdiseases/homePage.dart';
 
 void main() {
-  runApp(const Homepage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homepage(),
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tmtdiseases/main.dart';
+// import 'package:tmtdiseases/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
