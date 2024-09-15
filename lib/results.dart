68fe013d8f5ef035174addab35105c7a4b38cc7b
priyanjan <priyanjanjb@gmail.com>
2024-09-15 10:04:58 +0530

    Add button to result page

--- lib/results.dart
+++ lib/results.dart
@@ -52,7 +52,7 @@ class Results extends StatelessWidget {
               const Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(left: 15.0 ,right: 15.0),
+                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
                     child: SizedBox(
                       width: 345,
                       child: Column(children: [
@@ -63,11 +63,38 @@ class Results extends StatelessWidget {
                               fontWeight: FontWeight.bold, fontSize: 25),
                         ),
                         Text(
-                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+                        Text("Causes:",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                        ListBody(
+                          children: [
+                            Text("Lorem Ipsum is simply dummy text of "),
+                            Text("Lorem Ipsum is simply dummy text of "),
+                            Text("Lorem Ipsum is simply dummy text of "),
+                          ],
+                        ),
                       ]),
                     ),
                   )
                 ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 35.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton.extended(
+                      onPressed: null,
+                      label: Text("Chat With Us",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      backgroundColor: Color.fromARGB(255, 34, 177, 106),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
