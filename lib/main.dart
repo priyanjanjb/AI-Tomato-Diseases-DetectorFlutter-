86169a2b61f2b083529de2ba4cca12d0257221ad
priyanjan <priyanjanjb@gmail.com>
2024-09-18 11:44:58 +0530

    update splash sreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tmtdiseases/homePage.dart';
+import 'package:tmtdiseases/splashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       //home: Homepage(),
-      home: Homepage(),
+      home: Splashscreen(),
     );
   }
 }
---
+++ lib/splashScreen.dart
@@ -0,0 +1,38 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tmtdiseases/homePage.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AnimatedSplashScreen(
+        splash: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center the content
+          children: [
+            // Use Expanded or Flexible to fit the widgets within available space
+            const Expanded(
+              child: Image(
+                image: AssetImage(
+                  "assets/image/logo/_Green Simple Nature Beauty Care Initials Logo  (1).png",
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: LottieBuilder.asset(
+                'assets/animation/Animation - 1726379697327.json',
+                width: 204,
+              ),
+            ),
+          ],
+        ),
+        nextScreen: const Homepage(),
+        duration: 16000, // 16 seconds duration as you want
+      ),
+    );
+  }
+}
