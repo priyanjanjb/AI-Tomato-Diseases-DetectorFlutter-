8f0d893c9c6a17d29b4e94a44ffbc6ec3955b67a
priyanjan <priyanjanjb@gmail.com>
2024-11-07 08:03:41 +0530

    PixelUint8 error

--- lib/results.dart
+++ lib/results.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:tmtdiseases/treatment.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+// ignore: depend_on_referenced_packages
 import 'package:image/image.dart' as img;
 import 'dart:typed_data';
 
@@ -16,10 +17,43 @@ class Results extends StatefulWidget {
 
 class _ResultsState extends State<Results> {
   List<dynamic>? _predictions;
-  bool _isLoading = true;
+  bool _isLoading = false;
+
   late Interpreter _interpreter;
 
+  List<String> diseaseLabels = [
+    'Bacterial_spot',
+    'Early_blight',
+    'Late_blight',
+    'Leaf_Mold',
+    'Septoria_leaf_spot',
+    'Spider_Mites_(Two-Spotted_Spider_Mite)',
+    'Target_Spot',
+    'Yellow_Leaf_Curl_Virus',
+    'Mosaic_virus',
+    'Healthy'
+    // Add other labels...
+  ];
+
   final Map<String, Map<String, dynamic>> diseaseDetails = {
+    "Bacterial Spot": {
+      "description":
+          "A bacterial disease that causes small, water-soaked spots on leaves and fruits, leading to leaf yellowing and fruit blemishes.",
+      "causes": [
+        "Contaminated seeds or transplants.",
+        "Warm, wet conditions.",
+        "Splashing water from rain or irrigation."
+      ]
+    },
+    "Early Blight": {
+      "description":
+          "A fungal disease-causing brown spots with concentric rings on older leaves, eventually leading to leaf drops and fruit rot.",
+      "causes": [
+        "Overhead watering.",
+        "Warm, wet conditions.",
+        "Infected plant debris left in the soil."
+      ]
+    },
     "Late Blight": {
       "description":
           "A fungal disease that causes dark, water-soaked spots on leaves and fruits, eventually leading to plant death.",
@@ -27,6 +61,33 @@ class _ResultsState extends State<Results> {
         "Prolonged wet or humid conditions.",
         "Poor air circulation around plants.",
         "Infected seeds or transplants."
+      ]
+    },
+    "Leaf Mold": {
+      "description":
+          "A fungal disease causing pale green or yellow spots on the upper leaf surface, with velvety gray mold underneath.",
+      "causes": [
+        "High humidity and poor ventilation.",
+        "Water splashing on leaves.",
+        "Infected plant material."
+      ]
+    },
+    "Septoria Leaf Spot": {
+      "description":
+          "Fungal disease causing small, circular spots with gray centers and dark borders on leaves, leading to early defoliation.",
+      "causes": [
+        "Warm, wet weather.",
+        "Infected seeds or plants.",
+        "Water splashing from the soil onto leaves."
+      ]
+    },
+    "Spider Mites (Two-Spotted Spider Mite)": {
+      "description":
+          "Tiny pests that cause yellowing, speckled leaves, and fine webbing, eventually leading to leaf drop.",
+      "causes": [
+        "Hot, dry conditions.",
+        "Lack of natural predators.",
+        "Dusty, unclean growing environments."
       ]
     },
     "Target Spot": {
@@ -56,63 +117,20 @@ class _ResultsState extends State<Results> {
         "Seed-borne transmission."
       ]
     },
-    "Early Blight": {
-      "description":
-          "A fungal disease-causing brown spots with concentric rings on older leaves, eventually leading to leaf drops and fruit rot.",
+    "healthy": {
+      "description": "The plant is healthy and free from any diseases.",
       "causes": [
-        "Overhead watering.",
-        "Warm, wet conditions.",
-        "Infected plant debris left in the soil."
-      ]
-    },
-    "Spider Mites (Two-Spotted Spider Mite)": {
-      "description":
-          "Tiny pests that cause yellowing, speckled leaves, and fine webbing, eventually leading to leaf drop.",
-      "causes": [
-        "Hot, dry conditions.",
-        "Lack of natural predators.",
-        "Dusty, unclean growing environments."
-      ]
-    },
-    "Leaf Mold": {
-      "description":
-          "A fungal disease causing pale green or yellow spots on the upper leaf surface, with velvety gray mold underneath.",
-      "causes": [
-        "High humidity and poor ventilation.",
-        "Water splashing on leaves.",
-        "Infected plant material."
-      ]
-    },
-    "Septoria Leaf Spot": {
-      "description":
-          "Fungal disease causing small, circular spots with gray centers and dark borders on leaves, leading to early defoliation.",
-      "causes": [
-        "Warm, wet weather.",
-        "Infected seeds or plants.",
-        "Water splashing from the soil onto leaves."
-      ]
-    },
-    "Bacterial Spot": {
-      "description":
-          "A bacterial disease that causes small, water-soaked spots on leaves and fruits, leading to leaf yellowing and fruit blemishes.",
-      "causes": [
-        "Contaminated seeds or transplants.",
-        "Warm, wet conditions.",
-        "Splashing water from rain or irrigation."
+        "No known causes available.",
+        "Keep up the good work!",
+        "Continue to monitor plant health."
       ]
     }
   };
-
-  List<String> diseaseLabels = [
-    "Late Blight",
-    // Add other labels...
-  ];
 
   @override
   void initState() {
     super.initState();
     loadModel();
-    runModelOnImage();
   }
 
   Future<void> loadModel() async {
@@ -120,30 +138,52 @@ class _ResultsState extends State<Results> {
       _interpreter =
           await Interpreter.fromAsset('assets/model/tomatodiseas.tflite');
       print("Model loaded successfully.");
+      runModelOnImage();
     } catch (e) {
       print("Error loading model: $e");
       setState(() {
-        _isLoading = false;
+        _isLoading = true;
       });
     }
   }
 
   Future<void> runModelOnImage() async {
-    var inputImage = File(widget.imagePath).readAsBytesSync();
-    var input = await _preprocessImage(inputImage);
+    setState(() {
+      _isLoading = false;
+    });
 
-    var output = List<double>.filled(10, 0.0);
     try {
-      _interpreter.run(input, output);
-      print("Inference result: $output");
+      var inputImage = File(widget.imagePath).readAsBytesSync();
+      print("Image path: ${widget.imagePath}");
+
+      var input = await _preprocessImage(inputImage);
+      if (input.isEmpty) {
+        print("Error: Preprocessed input is empty.");
+        setState(() {
+          _isLoading = true;
+        });
+        return;
+      }
+
+      var output = List<double>.filled(diseaseLabels.length, 0.0);
+
+      try {
+        _interpreter.run(input, output);
+        print("Model prediction output: $output");
+      } catch (e) {
+        print("Error during model inference: $e");
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
 
       var predictedClassIndex =
           output.indexOf(output.reduce((a, b) => a > b ? a : b));
       var predictedLabel = diseaseLabels[predictedClassIndex];
-      var confidence = output[predictedClassIndex];
-
+      print("Predicted label: $predictedLabel");
       setState(() {
-        _predictions = [predictedLabel, confidence];
+        _predictions = [predictedLabel];
         _isLoading = false;
       });
     } catch (e) {
@@ -159,18 +199,28 @@ class _ResultsState extends State<Results> {
     img.Image? image = img.decodeImage(Uint8List.fromList(imageBytes));
     if (image == null) {
       return List.generate(
-          1, (_) => List.generate(224, (_) => List.generate(224, (_) => 0.0)));
+          1, (_) => List.generate(256, (_) => List.generate(256, (_) => 0.0)));
     }
 
-    img.Image resizedImage = img.copyResize(image, width: 224, height: 224);
-    List<List<List<double>>> normalizedImage = List.generate(224, (i) {
-      return List.generate(224, (j) {
+    // Resize image to the target size (256x256)
+    img.Image resizedImage = img.copyResize(image, width: 256, height: 256);
+
+    // Normalize pixel data
+    List<List<List<double>>> normalizedImage = List.generate(256, (i) {
+      return List.generate(256, (j) {
+        // Get the pixel value at (j, i)
         int pixel = resizedImage.getPixel(j, i) as int;
-        return [
-          (pixel & 0xFF) / 255.0,
-          ((pixel >> 8) & 0xFF) / 255.0,
-          ((pixel >> 16) & 0xFF) / 255.0
-        ];
+
+        // Extract RGB components from the pixel value
+        double red =
+            ((pixel >> 16) & 0xFF) / 255.0; // Extract red channel and normalize
+        double green = ((pixel >> 8) & 0xFF) /
+            255.0; // Extract green channel and normalize
+        double blue =
+            (pixel & 0xFF) / 255.0; // Extract blue channel and normalize
+
+        // Return the normalized RGB values
+        return [red, green, blue];
       });
     });
 
@@ -181,25 +231,17 @@ class _ResultsState extends State<Results> {
   Widget build(BuildContext context) {
     // Default values
     final String defaultDiseaseName = "No Disease Detected";
-    final String defaultConfidence = "0.00";
     final String defaultDescription = "No description available.";
     final List<String> defaultCauses = ["No known causes available."];
 
-// Use prediction data or default values
     final String diseaseName = _predictions != null && _predictions!.isNotEmpty
         ? _predictions![0].toString()
         : defaultDiseaseName;
 
-    final String confidence = _predictions != null && _predictions!.isNotEmpty
-        ? (100 * (_predictions![1] as double)).toStringAsFixed(2)
-        : defaultConfidence;
-
-// Get the disease description, or use the default if not found
     final String description = diseaseDetails.containsKey(diseaseName)
         ? (diseaseDetails[diseaseName]?['description'] ?? defaultDescription)
         : defaultDescription;
 
-// Get the disease causes list, or use the default if not found
     final List<String> causes = diseaseDetails.containsKey(diseaseName)
         ? List<String>.from(
             diseaseDetails[diseaseName]?['causes'] ?? defaultCauses)
@@ -231,6 +273,8 @@ class _ResultsState extends State<Results> {
                       : const Center(child: Text('No Image Selected')),
                 ),
               ),
+              // Display image name without extension
+
               _isLoading
                   ? const CircularProgressIndicator()
                   : Padding(
@@ -238,14 +282,12 @@ class _ResultsState extends State<Results> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Text("We’re highly confident that it is:",
+                              style: const TextStyle(fontSize: 20)),
                           Text(
-                            "Prediction: $diseaseName",
+                            diseaseName,
                             style: const TextStyle(
                                 fontWeight: FontWeight.bold, fontSize: 25),
-                          ),
-                          Text(
-                            "Confidence: $confidence%",
-                            style: const TextStyle(fontSize: 18),
                           ),
                           const SizedBox(height: 10),
                           const Text(
